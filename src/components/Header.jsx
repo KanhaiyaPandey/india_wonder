e6a78b7a927ea7ed5b4cfa6c3dd7023d308f5855
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2024-03-13 14:56:49 +0530

    redesign

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,26 @@
 const Header = () => {
   return (
     
-<section className=" h-screen bg-[#F5F7F8]" data-scroll-section>
+<section className=" h-[140vh]" data-scroll-section>
 
    <div className=" h-full w-full flex items-center justify-center">
-      <img src="/india-gate.jpg" alt="" className=" h-full w-full relative" />
-      <div className=" justify-center items-start absolute top-96 left-20 bottom-10 flex flex-col gap-3 text-white capitalize"
-      data-scroll data-scroll-direction="horizontal"
-      data-scroll-speed="-2">
+      <img src="/bg.jpg" alt="" className=" h-full w-full relative" />
 
-      <h1 className="  text-9xl font-bold">Namaste </h1>
-      <p className=" text-4xl font-semibold">To the land of diversity</p>
+     
+   
+      <div className=" justify-center items-center absolute top-0 mt-10 flex flex-col gap-3 text-white capitalize"
+        data-scroll data-scroll-direction="verticall"
+        data-scroll-speed="-4">
+      <h1 className=" text-9xl font-semibold z-10">Namaste</h1>
       </div>
+
+      <div className="justify-center items-start absolute top-96 left-20 bottom-10 flex flex-col gap-3 text-white capitalize">
+      <img src="/india-gate.png" alt="" className=" z-40 "
+             data-scroll data-scroll-direction="verticall"
+             data-scroll-speed="1" />
+      </div>
+
+     
    </div>
 </section>
  
