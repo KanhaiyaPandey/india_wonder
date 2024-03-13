5da98c6db10159e662ea7f37b304b07a78dd3ae1
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2024-03-13 16:19:14 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 
-
+import { motion } from "framer-motion"
 
 const Header = () => {
   return (
@@ -13,19 +13,22 @@ const Header = () => {
 
      
    
-      <div id="stick" className=" justify-center items-center absolute top-0 bottom-0 left-0 right-0 flex flex-col gap-3 text-black capitalize"
+      <motion.div
+      className=" justify-center items-center absolute top-16 z-10  flex flex-col gap-3 text-black capitalize"
+      data-scroll
+      data-scroll-direction="vertical"
+      data-scroll-speed="-6"
+      
         >
-      <h1 className=" text-9xl font-semibold z-10 absolute top-20"
-         data-scroll
-         data-scroll-speed="5"
-         data-scroll-sticky // Attibute that enables the sticky scroll
-         data-scroll-target="#stick">Namaste</h1>
-      </div>
+      <h1 className=" text-9xl font-semibold "
+        >Namaste</h1>
+        <p className=""></p>
+      </motion.div>
 
       <div className="justify-center items-center w-full h-full absolute top-52 left-0 bottom-10 right-0 flex">
-      <img src="/india-gate.png" alt="" className=" z-40 "
+      <img src="/india-gate.png" alt="" className=" w-[70rem] z-40 "
              data-scroll data-scroll-direction="verticall"
-             data-scroll-speed="1" />
+             data-scroll-speed="2" />
       </div>
 
      
